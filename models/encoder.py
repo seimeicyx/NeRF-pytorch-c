85dc7d115af5faa765bb7659e9a266dd09e93498
cad_83 <bemfoo@zju.edu.cn>
2023-05-30 22:45:19 +0800

    fix encoder problems

--- models/encoder.py
+++ models/encoder.py
@@ -6,14 +6,15 @@ import torch
 class FreEncoder():
     L:int
     out_dim:int=0
-    fns:List=[]
+    fns:List=field(default_factory=list,init=False)
     def __post_init__(self) -> Any:
-        fres=[np.sin,np.cos]
-        for i in range(self.L):
-            for fre in fres:
-                self.fns.append(lambda x:fre(2**i*np.pi*x))
-                self.out_dim+=3
+        self.fns=[]
+        fres=[torch.sin,torch.cos]
         self.fns.append(lambda x:x)
         self.out_dim+=3    
+        for i in range(self.L):
+            for fre in fres:
+                self.fns.append(lambda x:fre(2**i*torch.pi*x))
+                self.out_dim+=3
     def __call__(self,inputs:Any) -> Any:
         return torch.cat([fn(inputs) for fn in self.fns],dim=-1)
